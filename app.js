8cc625c20c0ebcabaf0f3db66c5d25b6687affa7
jimmyh3 <jimmyhe3452@gmail.com>
2018-08-13 20:06:53 -0700

    Added about page.

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const index = require('./routes/index');
 const users = require('./routes/users');
 const tests = require('./routes/tests');
 const games = require('./routes/games');
+const about = require('./routes/about');
 
 const app = express();
 app.io = require('./sockets');
@@ -85,6 +86,7 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/tests', tests);
 app.use('/games', games);
+app.use('/about', about);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) =>{
